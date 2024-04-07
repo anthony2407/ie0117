add2676dbf86a7e6c37f512bd2dcdb57098986f0
antony <antony.medinagarcia@ucr.ac.cr>
2024-04-07 15:54:29 -0600

    update : mejorando nombre de las variables a mayuscula

--- labo02/script_lab02.sh
+++ labo02/script_lab02.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # En esta variable se retorna el ultimo dia del mes en cuestion mediante el comando date y pasando sus opciones
-ultimodiadelmes=$(date -d "$(date +'%Y-%m-01')+1 month -1 day" '+%d')
+ULTIMO_DIA_MES=$(date -d "$(date +'%Y-%m-01')+1 month -1 day" '+%d')
 # Retorno del nombre  del dia actual
-dia=$(date '+%A')
+DIA=$(date '+%A')
 # Fecha actual 
-fecha=$(date '+%d')
+FECHA=$(date '+%d')
 #Mes actual
-mes=$(date '+%B')
+MES=$(date '+%B')
 # Anho ya que no me escribe la enhe
-anho=$(date '+%Y')
+ANHO=$(date '+%Y')
 # Calculo de los dias faltantes a partir del numero del ultimo dia menos el numero de dia actual
-diasfaltantes=$((ultimodiadelmes-fecha))
-mes_siguiente=$(date -d "+1 month" '+%B')
-echo "Hoy es $dia, $fecha de $mes, del año $anho, faltan $diasfaltantes dias para el mes de $mes_siguiente."
+DIAS_FALTANTES=$((ULTIMO_DIA_MES-FECHA))
+MES_SIGUIENTE=$(date -d "+1 month" '+%B')
+echo "Hoy es $DIA, $FECHA de $MES, del anho $ANHO, faltan $DIAS_FALTANTES dias para el mes de $MES_SIGUIENTE."
 
 
