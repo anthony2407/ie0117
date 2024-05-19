e3528b26bf325ab6a5be6bed7e917478a8659913
Antony <antony.medinagarcia24@gmail.com>
2024-05-19 17:57:59 -0600

    fix: mejorando el manejo de rrores con la función exit(1)

--- labo06/lab06.c
+++ labo06/lab06.c
@@ -5,15 +5,20 @@
 
 
 int main(int argc, char *argv[]) {
-    if (argc >3) {
-        printf("Error en la ejecución, el programa solo acepta dos entradas\n");// validación de argumentos
-        return 1; // terminar el programa
+    if (argc==1)
+    {
+        printf("Error en la ejecución, el programa necesita dos entradas de numeros enteros positivos\n");// validación de argumentos
+        exit(1); // terminar el programa
     }
-
+    else if (argc >3) {
+        printf("Error en la ejecución, el programa solo acepta dos entradas\n");// validación de argumentos
+        exit(1); // terminar el programa
+    }
+    
     int x = atoi(argv[1]); // convertir de string a entero
     int y = atoi(argv[2]); // convertir de string a entero
     int resultado = mcd(x, y); // llamada a la función mcd
-
+    
     printf("El máximo común divisor de %d y %d es: %d\n", x, y, resultado);
     return 0;
 }
--- labo06/mcdImplementado.c
+++ labo06/mcdImplementado.c
@@ -1,14 +1,15 @@
 #include "mcd.h" // incluir el archivo mcd.h donde se encuentra la declaración de la función mcd
 #include <stdio.h>
+#include <stdlib.h>
 // Acá se implementa la función mcd para ser utilizada en el archivo lab06.c
 int mcd(int x, int y) {
     if (x < 0 || y < 0) { // validacion que los ingresos sean mayores a 
         printf("No se puede ejecutar, el programa solo acepta enteros positivos\n");
-        return 1;
+        exit(1);
     }
     else if (x == 0 && y == 0) {
-        printf("No se puede ejecutar, ambos números no pueden ser cero en este programa.\n");
-        return 1;
+        printf("No se puede, ambos números no pueden ser cero en este programa.\n");
+        exit(1);
     }
     if (y == 0) return x; // caso base
     return mcd(y, x % y); // llamada recursiva a la función mcd 
